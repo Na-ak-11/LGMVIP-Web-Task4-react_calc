832378a94dd080b7c2ef511e9474b980220612b7
NahomAk <nahom.saketa108791@marwadiuniversity.ac.in>
2022-08-18 21:22:16 +0530

    ca

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import './App.css'
 
-function App() {
+const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const ops = [ '/', '*', '-', '+'];
+const ids = {
+              7: 'seven', 
+              8: 'eight', 
+              9: 'nine', 
+              4: 'four', 
+              5: 'five', 
+              6: 'six', 
+              1: 'one', 
+              2: 'two', 
+              3: 'three', 
+              0: 'zero',
+              '/': 'divide', 
+              '*': 'multiply', 
+              '-': 'subtract', 
+              '+': 'add'
+            }
+
+const App = () => {
+  const [lastPressed,setLastPressed] = useState(undefined);
+  const [calc, setCalc] = useState('0');
+
+  const handleClick = (e) => {
+    const { innerText } = e.target;
+
+    switch (innerText) {
+      case 'Clear': {
+        setCalc('0');
+        break;
+      }
+
+      case '=': {
+        const evaluated = eval(calc);
+        console.log(evaluated)
+        setCalc(evaluated);
+        break;
+      }
+
+      case '.': {
+        const splitted = calc.split(/[\+\-\*\/]/);
+        const last = splitted.slice(-1)[0];
+        if(!last.includes('.')) {
+            setCalc(calc +'.')
+        }
+        break;
+      }
+
+      default: {
+        let e = undefined;
+       
+        if(ops.includes(innerText)) {
+          if(ops.includes(lastPressed) && innerText !== '-') {
+         
+            const lastNumberIdx = calc.split('').reverse()
+            .findIndex(char => char !== ' ' && nums.includes(+char)); 
+            e = calc.slice(0, calc.length - lastNumberIdx) + ` ${innerText} `;
+          } else {
+            e = `${calc} ${innerText} `;
+          } 
+        }else {
+          e = (calc === '0') ? innerText : (calc + innerText);
+        }
+        
+        setCalc(e);
+        break;
+      }
+    }
+
+    setLastPressed(innerText);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div id="display" className="display">
+        
+        {calc}
+      </div>
+
+      <div className="nums-container">
+        <button className="clear" id="clear" onClick={handleClick}>Clear</button>
+
+        {nums.map(num => (
+          <button
+            className={`numbers ${num === 0 && 'zero'}`}
+            key={num}
+            onClick={handleClick}
+            id={ids[num]}
+          >
+            {num}
+          </button>
+        ))}
+
+        <button  className="numbers" onClick={handleClick} id="decimal"> .</button>
+        </div>
+
+      <div className="operators_container">
+        {ops.map(op =>(
+          <button 
+            className="operators"
+            key={op}
+            onClick={handleClick}
+            id={ids[op]}
+          >
+            {op}
+          </button>
+        ))}
+
+        <button className="equals" onClick={handleClick} id="equals"> =</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
